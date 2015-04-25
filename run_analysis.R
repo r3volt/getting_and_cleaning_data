0c3d0707f2b44ffbafb417356b1af27bbc2603ce
r3volt <chris@swatman-miller.com>
2015-04-25 00:11:02 -0400

    fully merged and trimmed data frame

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,4 @@
-# Check and create a data directory
-# Download the data zip file from: 
-#   https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-# Use unzip to extract the file in place
-# Use week3 Merging Data Lecture to merge into a single Data Frame
-# Create another Data Frame for mean and sd data
-# User dplyr rename to rename items
-# Proper variable names
-# Create another data frame using summarize
+library(dplyr)
 
 ## Function to identify, get and setup the source data for later analysis.
 setupSourceData<-function(workingDataDirectory="./data") {
@@ -34,14 +26,83 @@ setupSourceData<-function(workingDataDirectory="./data") {
   unzip(downloadFilePath, overwrite = TRUE, exdir=workingDataDirectory)
 }
 
+createDataSet<-function(dataDirectory="./data", dataSetName="train") {
+  sourceDataDirectoryName <- "UCI HAR Dataset"
+  dataNames <- c("subject", "X", "y")
+  ## Create initial data sets for train vs. test
+  ## Aim here is to create a complete merged set (subject + X + y)
+  ## for either test or train and then simply rbind() them.
+  ## The following will create a local environment variable for 
+  ## each of the data frames.
+  ##  subject_test, X_test, y_test OR
+  ##  subject_train, X_train, y_train
+  ## This is using eval and parse to make more modular.
+  for( i in 1:length(dataNames)) {
+    varName <- paste(dataNames[i], "_", dataSetName, sep = "")
+    fileName <- paste(varName, ".txt", sep="")
+    filePath <- paste(dataDirectory, sourceDataDirectoryName, dataSetName, fileName, sep="/")
+    cmd <- paste(varName, '<-read.table(\'', filePath, '\', header = FALSE)', sep = "")
+    eval(parse(text=cmd))
+  }
+  
+  ## Now we need to modify the names for (subject_{train, test})
+  cmd <- paste('names(subject_', dataSetName, ') <- c("subject_id")', sep = "")
+  eval(parse(text=cmd))
+  
+  ## Read the names of all variables in from "features.txt"
+  featuresFilePath<-paste(dataDirectory, sourceDataDirectoryName, "features.txt", sep = "/")
+  features<-read.table(featuresFilePath, col.names = c("id", "name"), header = FALSE)
+  ## To Ensure that names are correct use make.names
+  names<-make.names(names=features$name, unique = TRUE, allow_ = TRUE)
+  ## And then assign it names of (X_{train, test})
+  cmd <- paste('names(X_', dataSetName, ') <- names', sep = "")
+  eval(parse(text=cmd))
+  
+  ## Now we need to modify the names for (y_{train, test})
+  cmd <- paste('names(y_', dataSetName, ') <- c("activity_id")', sep = "")
+  eval(parse(text=cmd))
+
+#   cmd <- paste('df <- cbind(subject_', dataSetName, ', y_', dataSetName, ')', sep="")  
+  cmd <- paste('df <- cbind(subject_', dataSetName, ', X_', dataSetName, ', y_', dataSetName, ')', sep="")
+  eval(parse(text=cmd))
+
+  ## Last Step here will be to read the Activity Labels from the source file
+  activityLabelFilePath<-paste(dataDirectory, sourceDataDirectoryName, "activity_labels.txt", sep = "/")
+  activities<-read.table(activityLabelFilePath, col.names = c("id", "name"), header = FALSE)
+  ## And merge the values as another column (activity_name). See wk 3 lesson on how to accomplish this
+  ## We can use dplyr or factor levels
+  ## TODO: implement the activity label.
+
+  df
+}
+
 mergeDataSets<-function(mergeDataDirectory="./data") {
-  print("Hello there stupid!")
+#   sourceDataDirectoryName<-"UCI HAR Dataset"
+#   setNames<-c("train", "test")
+#   dataNames<-c("subject", "X", "y")
+#   fileName<-paste(dataNames[2], "_", setNames[1], ".txt", sep="")
+#   x_trainPath<-paste(mergeDataDirectory, sourceDataDirectoryName, setNames[1], fileName, sep="/")
+# 
+#   subject_train <- read.table(filePath, widths = lineFixedWidthDefinition, header = FALSE)
+#   X_train<-read.table(filePath, widths = lineFixedWidthDefinition, header = FALSE)
+#   
+# 
 }
 
 ## Main Run Steps
 dataDirectory<-"./data"
 setupSourceData(dataDirectory)
-mergeDataSets(dataDirectory)
+test <- createDataSet(dataDirectory, "test")
+train <- createDataSet(dataDirectory, "train")
 
+## Now merge the two data sets
+full<-rbind(test, train)
 
+# TODO: Extracts only the measurements on the mean and standard deviation for each measurement. 
+trimmed<-select(full, contains("mean"), contains("std"))
 
+# TODO: Use descriptive activity names to name the activities in the data set (done in step 1)
+
+# TODO: Appropriately labels the data set with descriptive variable names. 
+
+# TODO: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
